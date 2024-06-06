30b81f8e99f2d09e8bb5771511fdb26a4e497553
timp70294 <timp70294@gmail.com>
2024-06-06 12:03:42 -0500

    Copied code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+
+HttpClient client = new HttpClient();
+HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, "http://localhost:9191");
+
+//client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+HttpResponseMessage response = await client.SendAsync(message).ConfigureAwait(false);
+
+string context = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+Console.WriteLine(context);
+
+Console.WriteLine(response.StatusCode);
+Console.WriteLine(response); 
